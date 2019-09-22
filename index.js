88e23c698d303ed76a0d231b14fbf95470f882ee
hchiam <hchiam@users.noreply.github.com>
2019-09-22 17:58:57 -0400

    add comment to make it more obvious

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function showMessage() {
   const message = 'logging logging log';
 
   for (let i = 0; i < 5; i++) {
-    console.log(message)
+    console.log(message) // this will trigger underlining (and shows up if you run: npm run lint)
   }
 }
 
